c2b083df1b976ded274d157ddc79aafe2dab3922
Satenik <satenikkhachatryan55@gmail.com>
2021-08-12 12:11:09 +0400

    "Second assigment"

--- Classes/Data classes/src/Task.kt
+++ Classes/Data classes/src/Task.kt
@@ -1,4 +1,4 @@
-class Person
+data class Person(val name: String, val age: Int)
 
 fun getPeople(): List<Person> {
     return listOf(Person("Alice", 29), Person("Bob", 31))
@@ -9,3 +9,4 @@ fun comparePeople(): Boolean {
     val p2 = Person("Alice", 29)
     return p1 == p2  // should be true
 }
+
